97a17cfb21c2f166133965bf0b69be8a719435f8
pekyin5814 <pekyin@graduate.utm.my>
2021-10-31 22:56:54 -0700

    printing area only

--- testing.py
+++ testing.py
@@ -1,10 +1,13 @@
 import math
 
+#inserting the radius value
 radius_str = input ("Enter the radius: ")
 radius_int = int (radius_str)
 
+#calculating the circumference and area
 circum = 2 * math.pi * radius_int
 area = math.pi * (radius_int ** 2)
 
+#printing the area only
 print (" the circumference is: ", circum, \
 ", and the area is: ", area)
