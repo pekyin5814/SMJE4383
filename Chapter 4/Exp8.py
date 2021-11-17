c9d532ee7c360b6c1c95a9d2375ea06a84bb52d3
pekyin5814 <pekyin@graduate.utm.my>
2021-11-16 23:57:51 -0800

    update17112021

---
+++ Chapter 4/Exp8.py
@@ -0,0 +1,9 @@
+#open file for writing:
+#creates file if it does not exist
+#overwrite files if it exists
+
+>>>temp_file = open ("temp.txt" , "w")
+>>>print("first line", file=temp_file)
+>>>print("second line", file=temp_file)
+>>>temp_file.close()
+
